933e583c6232c79d0b92fa9d62ee223db31c6732
Frank <frank.lucassen@unibw.de>
2023-03-15 18:00:04 +0100

    hübschere ausgabe für gain

--- src/main/java/frank/lucassen/java/KI/EntropieGain.java
+++ src/main/java/frank/lucassen/java/KI/EntropieGain.java
@@ -3,8 +3,10 @@ package frank.lucassen.java.KI;
 import java.util.Arrays;
 
 public class EntropieGain {
+    private static final boolean printFull = true;
+
     public static void main(String[] args) {
-        // Blatt 5 Aufgabe 2
+        /*// Blatt 5 Aufgabe 2
         entropie(3, 3);
         gain(new int[]{3, 2, 1}, new int[]{3, 2, 1});
 
@@ -18,7 +20,16 @@ public class EntropieGain {
         gain(new int[]{9, 2, 4, 3}, new int[]{6, 4, 0, 2}); // vorhersage
         gain(new int[]{9, 2, 4, 3}, new int[]{6, 2, 3, 1}); // temp
         gain(new int[]{9, 3, 6}, new int[]{6, 4, 2}); // luftf
-        gain(new int[]{9, 6, 3}, new int[]{6, 3, 3}); // wind
+        gain(new int[]{9, 6, 3}, new int[]{6, 3, 3}); // wind*/
+
+        //gain(new int[]{4,2,2}, new int[]{4,2,2}); // klima
+        //gain(new int[]{4,3,1}, new int[]{4,2,2}); // meer
+        //gain(new int[]{4,4,0}, new int[]{4,2,2}); // kosten
+
+        // sonnig
+        gain(new int[]{2,1,1}, new int[]{4,4,0}); // temp warm, kalt
+        gain(new int[]{2,1,1}, new int[]{4,1,3}); // wind stark, schwach
+
     }
 
     public static double entropie(int p, int n) {
@@ -59,8 +70,18 @@ public class EntropieGain {
         for (int i = 1; i < p.length; i++) {
             result -= (1.0 * (p[i] + n[i]) / (p[0] + n[0])) * entropie(p[i], n[i], false);
         }
-        if (print)
-            System.out.println("gain für p=" + Arrays.toString(p) + " n=" + Arrays.toString(n) + " ist: " + result);
+        if (print) System.out.println("gain für p=" + Arrays.toString(p) + " n=" + Arrays.toString(n) + " ist: " + result);
+        if (printFull) {
+            System.out.println("Entropie(S) = e(p,n) = -p/(p+n) * log2(p/[p+n]) - n/(p+n) * log2(n/[p+n])");
+            System.out.print("gain für p=" + Arrays.toString(p) + " n=" + Arrays.toString(n) + " = ");
+            System.out.print("e(" + p[0]+","+n[0]+") - [");
+            for (int i = 1; i < p.length; i++) {
+                System.out.print((p[i] + n[i]) +"/"+ (p[0] + n[0]) + " * e("+p[i]+","+n[i]+")");
+                if (i != p.length-1) System.out.print(" + ");
+            }
+            System.out.println("] = " + result + "\n");
+
+        }
         return result;
     }
 }
